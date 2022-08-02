e1ca472ef0692cdd2b9594d4542065b32504fdcf
Pete Dodd <p.j.dodd@sheffield.ac.uk>
2022-08-02 20:13:10 +0100

    changing 65plus

--- R/plots.R
+++ R/plots.R
@@ -31,7 +31,6 @@ CLZ <- c('WHO'=who.col,'IHME'=ihme.col)
 
 ## load data
 load(here('data/prev.rda'))
-## load(here('data/agekey.rda'))
 load(here('data/hbc30key.rda'))
 load(here('data/N.rda'))
 
@@ -103,20 +102,21 @@ ASI <- ASI[,.(ihme=sum(val)),
 ALL <- merge(AS,ASI,by=c('iso3','sex','age'))
 ALL$age <- factor(ALL$age,levels=hagz,ordered=TRUE)
 ALL$sex <- factor(ALL$sex,levels=c('M','F'),ordered=FALSE)
-
+ALL$agey <- ALL$age
+ALL[age=='65plus',agey:='65+']
 
 ## -------------- incidence & notifications plot -------------
 ## incidence
-F1 <- ggplot(data=ALL,aes(x=age,y=newrel,fill=sex)) +
+F1 <- ggplot(data=ALL,aes(x=agey,y=newrel,fill=sex)) +
   coord_flip() +
   facet_wrap(~name,scales='free')+
   geom_bar(stat='identity',aes(y=ifelse(sex=='M',newrel,-newrel)))+
   xlab('Age group')+ylab('Incidence or notifications')+
   scale_y_continuous(labels = absspace)+
   geom_bar(stat='identity',
-           aes(x=age,y=ifelse(sex=='M',best,-best)),
+           aes(x=agey,y=ifelse(sex=='M',best,-best)),
            fill='transparent',col=1)+
-  geom_point(aes(x=age,y=ifelse(sex=='M',ihme,-ihme)),
+  geom_point(aes(x=agey,y=ifelse(sex=='M',ihme,-ihme)),
              size=2,col=1,
              shape=ifelse(ALL$ihme<ALL$newrel,16,1))+ #using solid points for CDR>1
   theme_light()+theme(legend.position = 'none')
@@ -129,16 +129,16 @@ ggsave(F1,file=here('plots/aF1.pdf'),h=ww*0.8,w=ww)
 ## same as above but singling out 1 country (BGD)
 tmp <- ALL[iso3=='BGD']
 
-F1 <- ggplot(data=tmp,aes(x=age,y=newrel,fill=sex)) +
+F1 <- ggplot(data=tmp,aes(x=agey,y=newrel,fill=sex)) +
   coord_flip() +
   facet_wrap(~name,scales='free')+
   geom_bar(stat='identity',aes(y=ifelse(sex=='M',newrel,-newrel)))+
   xlab('Age group')+ylab('Incidence or notifications')+
   scale_y_continuous(labels = absspace)+
   geom_bar(stat='identity',
-           aes(x=age,y=ifelse(sex=='M',best,-best)),
+           aes(x=agey,y=ifelse(sex=='M',best,-best)),
            fill='transparent',col=1)+
-  geom_point(aes(x=age,y=ifelse(sex=='M',ihme,-ihme)),
+  geom_point(aes(x=agey,y=ifelse(sex=='M',ihme,-ihme)),
              size=2,col=1,
              shape=ifelse(tmp$ihme<tmp$newrel,16,1))+
   theme_light()+theme(legend.position = 'none')
--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # ihmexplore
-
-comments back:
-- PN for IHME in f2a (including HIV+)
-- horizontal line?
-- larger text
